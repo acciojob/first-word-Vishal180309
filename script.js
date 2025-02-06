4d04f72feb5bdf164021a6d17403aa4cabfe7a1f
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-06 12:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function firstWord(s) {
-  // your code here
+document.getElementById('find-button').addEventListener('click', function() {
+    let inputString = document.getElementById('input-string').value;
+    let result = firstWord(inputString);
+
+    document.getElementById('result').innerHTML = 'First word: ' + result;
+});
+
+function firstWord(str) {
+    let spaceIndex = str.indexOf(' ');
+
+    if (spaceIndex === -1) {
+        return str;
+    } else {
+        return str.substring(0, spaceIndex);
+    }
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
